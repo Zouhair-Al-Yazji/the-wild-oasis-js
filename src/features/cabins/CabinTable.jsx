96eb64b9a1f4f1197725f8c538537ba123ddd40d
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-05-08 11:34:08 +0300

    client-side filtering, sorting cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,13 +3,30 @@ import CabinRow from './CabinRow';
 import Spinner from '../../ui/Spinner';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
+import { useSearchParams } from 'react-router-dom';
 
 export default function CabinTable() {
 	const { isPending, isError, error, cabins } = useCabins();
+	const [searchParams] = useSearchParams();
+
+	// 1. FILTER
+	const filterValue = searchParams.get('discount') || 'all';
+
+	let filteredCabins;
 
 	if (isPending) return <Spinner />;
 
 	if (isError) return <p>{error.message}</p>;
+
+	if (filterValue === 'all') filteredCabins = cabins;
+	if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0);
+	if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0);
+
+	// 2. SORT
+	const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+	const [field, direction] = sortBy.split('-');
+	const modifier = direction === 'asc' ? 1 : -1;
+	const sortedCabins = filteredCabins.sort((a, b) => (a[field] - b[field]) * modifier);
 
 	return (
 		<Menus>
@@ -22,7 +39,10 @@ export default function CabinTable() {
 					<div role="cell">discount</div>
 					<div role="cell"></div>
 				</Table.Header>
-				<Table.Body data={cabins} render={cabin => <CabinRow cabin={cabin} key={cabin.id} />} />
+				<Table.Body
+					data={sortedCabins}
+					render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
+				/>
 			</Table>
 		</Menus>
 	);
