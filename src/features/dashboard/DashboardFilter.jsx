7cc95a7b1cb1eb4155c1c3d28fdb8fcb896ea542
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-08-02 15:16:11 +0300

    update filter text

--- src/features/dashboard/DashboardFilter.jsx
+++ src/features/dashboard/DashboardFilter.jsx
@@ -7,7 +7,7 @@ function DashboardFilter() {
 			options={[
 				{ value: '7', label: 'Last 7 days' },
 				{ value: '30', label: 'Last 30 days' },
-				{ value: '90', label: 'Last 90 days' },
+				{ value: '90', label: 'Last 3 months' },
 			]}
 		/>
 	);
